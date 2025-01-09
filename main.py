d7e7d0f2bb0628d979ec53101c9d06d590434b3e
Zhongyang Hu <1016509070@qq.com>
2025-01-09 15:53:33 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,72 @@
+import sys
+import torch
+import os
+import random
+import os
+import numpy as np
+import time
+import logging
+import json
+
+from config import Config
+
+logging.basicConfig(level=logging.INFO, format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+
+
+
+def choose_optimizer(config, model):
+    optimizer = config['optimizer']
+    learning_rate = config['learning_rate']
+    
+    if optimizer == 'adam'
+        return torch.optim.Adam(param = model.parameters(), lr = learning_rate)
+	elif optimizer == 'sgd':
+		return torch.optim.Adam(param = model.parameters(), lr = learning_rate)
+
+
+
+
+
+
+
+
+
+def main(config):
+    #创建保存模型的目录
+
+
+
+
+	# 加载模型
+
+
+
+	# gpu transfer
+
+
+
+	# optimizer
+
+
+	# training data
+
+
+	# evaluator
+
+
+	# loss
+
+
+
+	# start training
+
+
+	
+
+
+
+if __name__ == '__main__':
+    main(Config)
