efb39d4768c659b0101336d4605370c4bf48b2cc
Zhongyang Hu <1016509070@qq.com>
2025-01-09 17:33:57 +0800

    Create Modules.py

---
+++ transformer/Modules.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+__author__ = "Yu-Hsiang Huang"
+
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, temperature, attn_dropout=0.1):
+        pass
+
+    def forward(self, x):
+        pass
