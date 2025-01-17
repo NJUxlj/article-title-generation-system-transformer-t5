2d6d08ecc5f577e8e5ede27e1779653fe5ce63e3
NJUxlj <1016509070@qq.com>
2025-01-17 12:30:11 +0800

    update

--- transformer/Layers.py
+++ transformer/Layers.py
@@ -24,7 +24,7 @@ class EncoderLayer(nn.Module):
 
 
 
-
+    
 
 
 
--- transformer/Models.py
+++ transformer/Models.py
@@ -10,19 +10,50 @@ class PositionalEncoding(nn.Module):
     
 
     def __init__(self, d_hid, n_position=200):
-        pass
+        '''
+        '''
+        # Add a buffer to the module.
+        # buffer 中存放的是当前模块中的一些状态值，而非参数
+        self.register_buffer('pos_table', self._get_sinusoid_encoding_table(n_position, d_hid))
+        '''
 
+        register_buffer 方法的主要作用有以下几点：
 
-    def _get_sinusoid_encoding_table():
-        
+            存储模型状态：缓冲区通常用于存储模型的状态信息，这些信息在模型的训练和推理过程中需要保持不变，
+                        但又不是模型的可学习参数。例如，在循环神经网络（RNN）中，隐藏状态（hidden state）通常被存储在缓冲区中。
+
+            避免梯度计算：由于缓冲区不是模型的参数，它们不会参与梯度计算。
+                        这意味着在反向传播过程中，缓冲区不会被更新，从而节省了计算资源。
+
+            模型保存和加载：缓冲区会随着模型一起被保存和加载。这使得模型在重新加载时能够恢复到之前的状态，包括缓冲区中的数据。
+
+        这行代码注册了一个名为 pos_table 的缓冲区，它存储了正弦位置编码表。
+                    这个表在模型的前向传播过程中会被使用，
+                    用于为输入序列中的每个位置添加位置信息，但它本身不会被优化器更新。
+        '''
+
+    def _get_sinusoid_encoding_table(self, n_positions, d_hid):
+        ''' Sinusoid position encoding table '''
 
         def get_position_angle_vec(position):
-            pass
+            return [position/(10000**(2*(i//2)/d_hid)) for i in range(d_hid)]
 
+        # sinusoid_table.shape = (n_positions, d_hid)
+        sinusoid_table = np.array([get_position_angle_vec(pos_i) for pos_i in range(n_positions)])
+
+        # Apply sin to even indices along the d_hid dimension for each position vector; 2i
+        sinusoid_table[:,0::2] = np.sin(sinusoid_table[:,0::2])   # dim 2i
+        sinusoid_table[:,1::2] = np.sin(sinusoid_table[:, 1::2])  # dim 2i+1
+
+        # clone().detach(): 创建一个独立于原始张量的新张量，并在计算图中断开连接（禁用梯度计算）
+        return torch.FloatTensor(sinusoid_table).unsqueeze(0)
 
 
     def forward(self, x):
-        pass
+        '''
+        x.shape = (B, L, d_model)
+        '''
+        return x+ self.pos_table(x[:,:x.size(1),:]).clone().detach()
 
 
 
@@ -34,10 +65,10 @@ class Encoder(nn.Module):
             self, n_src_vocab, d_word_vec, n_layers, n_head, d_k, d_v,
             d_model, d_inner, pad_idx, dropout=0.1, n_position=200, scale_emb=False):
         super().__init__()
-        self.src_word_embed = nn.Embedding(n_src_vocab, d_word_vec, padding_idx=pad_idx)
+        self.src_word_emb = nn.Embedding(n_src_vocab, d_word_vec, padding_idx=pad_idx)
         self.position_enc = PositionalEncoding()
         self.layer_stack = nn.ModuleList([
-            EncoderLayer()
+            EncoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout)
             for _ in range(n_layers)
         ])
 
@@ -46,11 +77,31 @@ class Encoder(nn.Module):
         self.scale_emb = scale_emb
         self.d_model = d_model
 
-    def forward(self,):
-        pass
+    def forward(self, src_seq, src_mask, return_attns=False):
+        '''
+        :param src_seq: padded input sequence of shape (B, L, d_model)
+        :param src_mask: source sequence mask of shape (B, L)
+        '''
+        enc_slf_attn_list = []
 
+        # -- Forward
+        enc_output = self.src_word_emb(src_seq)
 
+        if self.scale_emb:
+            enc_output*= self.d_model*0.5
+        
 
+        enc_output = self.dropout(self.position_enc(enc_output))
+        enc_output = self.layer_norm(enc_output)
+
+        for enc_layer in self.layer_stack:
+            enc_layer: EncoderLayer
+            enc_output, enc_slf_attn = enc_layer.forward(enc_output, slf_attn_mask=src_mask)
+            enc_slf_attn_list += [enc_slf_attn] if return_attns else []
+
+        if return_attns:
+            return enc_output, enc_slf_attn_list
+        return enc_output
 
 
 
@@ -70,7 +121,7 @@ class Decoder(nn.Module):
 
         self.layer_stack = nn.ModuleList(
             [
-              DecoderLayer()  for _ in range(n_layers)
+              DecoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout)  for _ in range(n_layers)
             ]
         )
 
